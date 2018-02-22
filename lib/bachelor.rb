0e46b6f9558271690787dda401c796615d403fc8
Harim Choi <harim1206@gmail.com>
2018-02-22 23:01:14 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -691,7 +691,7 @@ def count_contestants_by_hometown(data, hometown)
   contestants_arr.each{
     |contestant|
 
-    binding.pry
+    #binding.pry
     if contestant[:hometown] == hometown
       counter += 1
     end
