3336d53285d13a00ae59504424f59bcf330315c4
Harim Choi <harim1206@gmail.com>
2018-02-22 22:53:59 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -670,7 +670,7 @@ end
 all_contestants(example_JSON)
 
 
-end
+
 #Build a method, count_contestants_by_hometown, that takes in two arguments––the data
 # hash and a string of a hometown. This method should return a counter
 # of the number of contestants who are from that hometown.
