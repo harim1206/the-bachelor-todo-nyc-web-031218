c442084870cc01cbe964e0d707f8863d8bac820d
Harim Choi <harim1206@gmail.com>
2018-02-22 23:07:17 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -731,7 +731,12 @@ end
 
 
 
-
+#Build a method, get_average_age_for_season, that takes in two arguments––the data hash and 
+#a string of a season. Iterate through the hash and return the average age of all of
+# the contestants for that season.
 def get_average_age_for_season(data, season)
   # code here
+  data.each{
+    |data_season, array|
+  }
 end
