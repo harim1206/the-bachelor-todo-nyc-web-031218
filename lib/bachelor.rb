5f5e03219ea6d6fda15b63302ab025926309dde1
Harim Choi <harim1206@gmail.com>
2018-02-22 23:08:46 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -742,6 +742,7 @@ def get_average_age_for_season(data, season)
     if data_season == season
 
       season_num_contestants = array.length
+      total_age_sum = 0
       array.each{
         |contestant|
         contestant["age"]
